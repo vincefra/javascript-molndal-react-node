12e5713a64876c638970ab3b302b935b82fca80e
Vincent <vincent.fra@icloud.com>
2021-03-05 23:18:38 +0100

    added morgan, helmet, dotenv and changed to modules

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,28 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+dotenv.config() //instansiera
 
 const app = express()
+app.use(helmet())
+app.use(morgan('common'))
 
-app.listen(3001, () => {
-    console.log("test")
+const port = process.env.PORT //för nå variabeln port i env
+
+/*
+app.post //create
+app.put //update
+app.delete  //delete
+app.get //read
+*/
+
+//req, ta emot, res skicka
+app.get("/product", (req, res) => {
+    res.send("Yaay!")
+})
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
 })
